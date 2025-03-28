31763865f8e684a163c8ac1a1dba590cad797259
Brunoenr02 <brunoancco02@gmail.com>
2025-03-27 23:29:40 -0500

    Actividad_01

--- Bank/Bank.Domain.Tests/BankAccountTests.cs
+++ Bank/Bank.Domain.Tests/BankAccountTests.cs
@@ -19,5 +19,32 @@ namespace Bank.Domain.Tests
             double actual = account.Balance;
             NUnit.Framework.Assert.That(actual, Is.EqualTo(expected).Within(0.001), "Account not debited correctly");
         }
+
+        [Test]
+        public void Credit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double creditAmount = 5.00;
+            double expected = 16.99;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Credit(creditAmount);
+
+            // Assert
+            double actual = account.Balance;
+            NUnit.Framework.Assert.That(actual, Is.EqualTo(expected).Within(0.001), "Account not credited correctly");
+        }
+
+        [Test]
+        public void Credit_WithNegativeAmount_ThrowsException()
+        {
+            // Arrange
+            BankAccount account = new BankAccount("Mr. Bryan Walton", 11.99);
+
+            // Act & Assert
+            NUnit.Framework.Assert.Throws<ArgumentOutOfRangeException>(() => account.Credit(-5.00));
+        }
     }
 }
